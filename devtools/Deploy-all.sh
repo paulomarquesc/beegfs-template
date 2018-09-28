2e6429c2a94fa627752eef8874a863160344e465
paulomarquesc <pmarques@microsoft.com>
2018-09-28 10:01:05 -0700

    Chaging test deployment all script

--- devtools/Deploy-all.sh
+++ devtools/Deploy-all.sh
@@ -1,8 +1,8 @@
 # Deploying management nodes
-./Deploy-AzureResourceGroup.sh -g beegfs-rg-eus -l eastus -t ../deploy-beegfs-master.json -p ../deploy-beegfs-master-parameters.json -s pmcstorage08 -r support-rg
+../Deploy-AzureResourceGroup.sh -g beegfs-rg-eus -l eastus -t ../deploy-beegfs-master.json -p ../deploy-beegfs-master-parameters.json -s pmcstorage08 -r support-rg
 
 # Deploying server nodes (meta and storage)
-./Deploy-AzureResourceGroup.sh -g beegfs-rg-eus -l eastus -t ../deploy-beegfs-nodes.json -p ../deploy-beegfs-nodes-parameters.json -s pmcstorage08 -r support-rg
+../Deploy-AzureResourceGroup.sh -g beegfs-rg-eus -l eastus -t ../deploy-beegfs-nodes.json -p ../deploy-beegfs-nodes-parameters.json -s pmcstorage08 -r support-rg
 
 # Deploying clients with SAMBA
-./Deploy-AzureResourceGroup.sh -g beegfs-rg-eus -l eastus -t ../deploy-clients.json -p ../deploy-clients-parameters.json -s pmcstorage08 -r support-rg
+../Deploy-AzureResourceGroup.sh -g beegfs-rg-eus -l eastus -t ../deploy-clients.json -p ../deploy-clients-parameters.json -s pmcstorage08 -r support-rg
